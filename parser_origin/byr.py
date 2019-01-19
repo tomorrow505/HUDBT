96de4b024c628eade7f776a66bb3a43516a63a3b
tomorrow505 <32202634+tomorrow505@users.noreply.github.com>
2019-01-19 21:22:06 +0800

    Update byr.py

--- parser_origin/byr.py
+++ parser_origin/byr.py
@@ -29,12 +29,12 @@ def parser_html(html, torrent_path):
     descr = to_bbcode(str(soup.find('div', id='kdescr')))
     try:
         # 这里检验的re的pattern有点问题，不过可以用，如果有豆瓣信息，自己构造简介
-        link = re.search('.*douban.com/subject/(\d{8})', descr)
+        link = re.search('.*douban.com/subject/(\d{7,8})', descr)
         link = ('https://movie.douban.com/subject/'+link.group(1)+'/')
         descr = get_descr.get_full_descr(link, torrent_path)
     except Exception:
         try:
-            link_1 = re.search('.*imdb.com/title/(tt\d{7, 8})', descr)
+            link_1 = re.search('.*imdb.com/title/(tt\d{7,8})', descr)
             link_1 = 'https://www.imdb.com/title/'+link_1.group(1)+'/'
             descr = get_descr.get_full_descr(link_1, torrent_path)
         except Exception:
